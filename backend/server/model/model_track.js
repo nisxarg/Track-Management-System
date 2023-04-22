d02e889cb57d67bb37a83a705f9c34b75234b6ad
Vyom Patel <202001045@daiict.ac.in>
2023-04-22 18:19:46 +0530

    models.track.js

--- backend/server/model/model_track.js
+++ backend/server/model/model_track.js
@@ -2,33 +2,103 @@ const mongoose = require('mongoose')
 
 var track_schema = new mongoose.Schema({
 
-    name:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    year:{
-        type: String,
-        required: true,
-    },
-    introduction:{
-        type: String,
-        required: true
-    },
-    description:{
-        type: String,
-        required: true
-    },
-    submission_format:{
-        type: String,
-        required: true
-    },
-    evalution_criteria:{
-        type: String,
-        required: true        
-    }
+    sidebar: {
+        title: {
+            type: String,
+            required: true
+        },
+        links: [{
+            name: {
+                type: String,
+                required: true
+            },
 
-}) 
+        }]
+    },
+    // type: String,
+    // required: true,
+    // unique: true
+    //},
+    importantDates: {
+        title: {
+            type: String,
+            required: true
+        },
+        dates: [{
+            date: {
+                type: String,
+                required: true
+            },
+            event: {
+                type: String,
+                required: true
+            }
+        }]
+    },
+
+    Content: {
+        introduction: {
+            title: {
+                type: String,
+                required: true
+            },
+            content: {
+                type: String,
+                required: true
+            }
+        },
+        TaskDescription: {
+            title: {
+                type: String,
+                required: true
+            },
+            content: {
+                type: String,
+                required: true
+            }
+        },
+        corpus: {
+            title: {
+                type: String,
+                required: true
+            },
+            content: {
+                type: String,
+                required: true
+            }
+        },
+        registration: {
+            title: {
+                type: String,
+                required: true
+            },
+            content: {
+                type: String,
+                required: true
+            }
+        },
+        submission: {
+            title: {
+                type: String,
+                required: true
+            },
+            content: {
+                type: String,
+                required: true
+            }
+        },
+        evaluation: {
+            title: {
+                type: String,
+                required: true
+            },
+            content: {
+                type: String,
+                required: true
+            }
+        }
+    }
+})
 
 //creating collection
 const trackdb = mongoose.model('trackdb', track_schema);  //(<collectionname>, <collectionshema>)
