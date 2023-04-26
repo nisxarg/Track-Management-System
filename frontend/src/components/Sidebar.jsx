b1c47f5a569f126324d61db36577a8c9bfe7b198
Pranav210401 <202001055@daiict.ac.in>
2023-04-26 12:21:40 +0530

    sidebar navigation issue solved

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,9 +6,9 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 // import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
- // change
+// change
 import { useLocation } from 'react-router-dom';
-import { Track } from '../data/dummy'; 
+import { Track } from '../data/dummy';
 import PropTypes from 'prop-types';
 // change
 
@@ -20,11 +20,11 @@ const Sidebar = () => {
     screenSize,
     sidebarData,
   } = useStateContext();
-   // change
+  // change
   const location = useLocation();
   const isTrackDetailsPage = location.pathname.startsWith('/track-details');
   const sidebarOptions = isTrackDetailsPage ? Track.sidebar.links : [];
-   // change
+  // change
 
   const handleCloseSideBar = () => {
     if (activeMenu !== undefined && screenSize <= 900) {
@@ -39,80 +39,85 @@ const Sidebar = () => {
     'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
 
-const links=sidebarData;
-return (
-  <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-    {activeMenu && (
-      <>
-        <div className="flex justify-start items-center">
-          <Link
-            to="/"
-            onClick={handleCloseSideBar}
-            className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
-          >
-            <SiShopware />
-            <span>FIRE</span>
-          </Link>
-          <TooltipComponent content="Menu" position="BottomCenter">
-            <button
-              type="button"
-              onClick={() => setActiveMenu(!activeMenu)}
-              style={{ color: currentColor }}
-              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+  const links = sidebarData;
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-start items-center">
+            <Link
+              to="/"
+              onClick={handleCloseSideBar}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <MdOutlineCancel />
-            </button>
-          </TooltipComponent>
-        </div>
-        <div className="mt-10 ">
-        {!isTrackDetailsPage &&
-  links.map((item) => (
-    <div key={item.title}>
-      <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
-        {item.title}
-      </p>
-      {item.links.map((link) => (
+              <SiShopware />
+              <span>FIRE</span>
+            </Link>
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => setActiveMenu(!activeMenu)}
+                style={{ color: currentColor }}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className="mt-10 ">
+            {!isTrackDetailsPage &&
+              links.map((item) => (
+                <div key={item.title}>
+                  <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                    {item.title}
+                  </p>
+                  {item.links.map((link) => (
+                    <NavLink
+                      to={`/${link.name}`}
+                      key={link.name}
+                      onClick={(event) => {
+                        event.preventDefault();
+                        window.location.href = `/${link.name}`;
+                        handleCloseSideBar();
+                      }}
+                      style={({ isActive }) => ({
+                        backgroundColor: isActive ? currentColor : '',
+                      })}
+                      className={({ isActive }) => (isActive ? activeLink : normalLink)}
+                      exact
+                    >
+                      {link.icon}
+                      <span className="capitalize ">{link.name}</span>
+                    </NavLink>
+                  ))}
+                </div>
+              ))}
+          </div>
+        </>
+      )}
+      {/* change */}
+      {/* Render the sidebar options */}
+      {sidebarOptions.map((option) => (
         <NavLink
-          to={`/${link.name}`}
-          key={link.name}
+          to={`/${option.name}`}
+          key={option.name}
           onClick={handleCloseSideBar}
           style={({ isActive }) => ({
             backgroundColor: isActive ? currentColor : '',
           })}
           className={({ isActive }) => (isActive ? activeLink : normalLink)}
         >
-          {link.icon}
-          <span className="capitalize ">{link.name}</span>
+          {option.icon}
+          <span className="capitalize ">{option.name}</span>
         </NavLink>
-      ))}
-      </div>
-    ))}
-  </div>
-</>
-)}
-  {/* change */}
-  {/* Render the sidebar options */}
-  {sidebarOptions.map((option) => (
-    <NavLink
-      to={`/${option.name}`}
-      key={option.name}
-      onClick={handleCloseSideBar}
-      style={({ isActive }) => ({
-        backgroundColor: isActive ? currentColor : '',
-      })}
-      className={({ isActive }) => (isActive ? activeLink : normalLink)}
-    >
-      {option.icon}
-      <span className="capitalize ">{option.name}</span>
-    </NavLink>
-    
-  ))}
-  {/* change */}
-  
-</div>
 
-);
-        }
+      ))}
+      {/* change */}
+
+    </div>
+
+  );
+}
 export default Sidebar;
 
 // Sidebar.propTypes = {
