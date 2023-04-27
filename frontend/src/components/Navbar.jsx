6456f7df94c4886d68dc68e620e62ad448ea9f54
Ajeya <ajeya.mm@gmail.com>
2023-04-27 15:21:22 +0530

    Resolved Notification icon GUI issue

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -112,7 +112,7 @@ const Navbar = () => {
               <div className="flex">
 
                 <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => {}} color={currentColor} icon={<BsChatLeft />} />
-                <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />} />
+                <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => {}} color={currentColor} icon={<RiNotification3Line />} />
                 {localStorage.getItem("token") ?
                   (
                     <div
